85d465678492384c88791137e81ee01e3552d9f8
uniqcle <uniqcle@yandex.ru>
2022-01-27 10:25:17 +0700

    debugging redux app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import store from './store'
+import * as actions from './actions'
+
+store.subscribe(() => {
+    console.log('subscribe...')
+})
+
+store.dispatch(actions.bugAdded("bugging 1"))
+store.dispatch(actions.bugAdded("bugging 2"))
+store.dispatch(actions.bugAdded("bugging 3"))
+store.dispatch(actions.bugResolved(1))
+
+console.log(store.getState())
+
+/*
+import store from './store'
 import { bugAdded, bugRemoved, bugResolved } from './actions';
 
 const unsubscribe = store.subscribe(() => {
@@ -13,3 +28,4 @@ unsubscribe();
 store.dispatch(bugRemoved(1))
 
 console.log(store.getState())
+*/ 
