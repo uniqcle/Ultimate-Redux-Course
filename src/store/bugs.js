883cf01195832e47d22607148cc158095759a67b
uniqcle <uniqcle@yandex.ru>
2022-01-27 17:01:26 +0700

    slice

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,37 +1,37 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
-const bugUpdated = createAction("bugUpdated")
-console.log(bugUpdated({ id: 1 }))
-
-//Action creators
-export const bugAdded = createAction("bugAdded")
-export const bugResolved = createAction("bugResolved")
-export const bugRemoved = createAction("bugRemoved")
-
-//Reducer
 let lastId = 0;
 
+const slice = createSlice({
+    name: 'bugs',
+    initialState: [],
+    reducers: {
+        //actions => action handlers
+        bugAdded: (bugs, action) => {
+            bugs.push({
+                id: ++lastId,
+                description: action.payload.description,
+                resolved: false
+            })
+        },
 
-export default createReducer([], {
-    //key: value
-    //actions: function(event => event handler)
-    [bugAdded.type]: (bugs, action) => {
-        bugs.push({
-            id: ++lastId,
-            description: action.payload.description,
-            resolved: false
-        })
-    },
+        bugResolved: (bugs, action) => {
+            const index = bugs.findIndex(bug => bug.id === action.payload.id)
+            bugs[index].resolved = true;
+        },
 
-    [bugResolved.type]: (bugs, action) => {
-        const index = bugs.findIndex(bug => bug.id === action.payload.id)
-        bugs[index].resolved = true;
-    },
-
-    [bugRemoved.type]: (bugs, action) => {
-        return bugs.filter(bug => bug.id !== action.payload.id)
+        bugRemoved: (bugs, action) => {
+            bugs.filter(bug => bug.id !== action.payload.id)
+        }
     }
 })
+
+export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
+export default slice.reducer;
+
+
+
+
 
 
 /*
