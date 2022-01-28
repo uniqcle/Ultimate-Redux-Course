ab934b2f036dea16885d1cb4f0cd1b7a5dfa2664
uniqcle <uniqcle@yandex.ru>
2022-01-28 14:12:33 +0700

    project slice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import configureStore from './user_store_toolkit/configureStore'
-import * as actions from './user_store_toolkit/users'
+import configureStore from './store_projects/configureStore'
+import * as actions from './store_projects/projects'
 
 const store = configureStore();
 
@@ -7,8 +7,13 @@ const unsubscribe = store.subscribe(() => {
     console.log("store is changing...")
 })
 
-store.dispatch(actions.userAdded({ id: 5 }))
-store.dispatch(actions.userRemoved({ id: 5 }))
+store.dispatch(actions.projectAdded({ id: 3 }))
+store.dispatch(actions.projectAdded({ id: 4 }))
+store.dispatch(actions.projectAdded({ id: 5 }))
+store.dispatch(actions.projectAdded({ id: 6 }))
+store.dispatch(actions.projectAdded({ id: 7 }))
 
-store.dispatch(actions.userAdded({ id: 6 }))
-store.dispatch(actions.userIsAdmin({ id: 6 }))
+store.dispatch(actions.projectInHand({ id: 3 }))
+store.dispatch(actions.projectRemoved({ id: 3 }))
+
+console.log(store.getState())
---
+++ src/store_projects/projects.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const slice = createSlice({
+    name: 'projects',
+    initialState: [],
+    reducers: {
+        projectAdded: (projects, action) => {
+            projects.push({
+                id: action.payload.id,
+                name: action.payload.name,
+                inHand: false
+            })
+        },
+
+        projectRemoved: (projects, action) => {
+            //projects.filter((pro) => pro.id == action.payload.id)
+            const index = projects.findIndex(p => p.id === action.payload.id);
+            if (index !== -1) {
+                projects.splice(index, 1);
+            }
+        },
+
+        projectInHand: (projects, action) => {
+            const index = projects.findIndex(p => p.id === action.payload.id)
+            projects[index].inHand = true;
+        }
+    }
+})
+
+export const { projectAdded, projectInHand, projectRemoved } = slice.actions;
+export default slice.reducer; 
