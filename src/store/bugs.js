3bbbf4e74b65133389977a0f251dd3bac6cc2127
uniqcle <uniqcle@yandex.ru>
2022-02-03 16:21:23 +0700

    caching

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { createSelector } from 'reselect'
 import { apiCallBegan } from './api'
+import moment from 'moment'
 
 let lastId = 0;
 
@@ -20,6 +21,7 @@ const slice = createSlice({
         bugsReceived: (bugs, action) => {
             bugs.list = action.payload;
             bugs.loading = false;
+            bugs.lastFetch = Date.now()
         },
 
         bugAdded: (bugs, action) => {
@@ -75,13 +77,22 @@ export default slice.reducer;
 //Action Creators
 const url = "/bugs"
 
-export const loadBugs = () => apiCallBegan({
-    url,
-    onStart: bugsRequested.type,
-    onSuccess: bugsReceived.type,
-    onError: bugsRequestFailed.type
-    //onError: actions.apiCallFailed.type
-})
+export const loadBugs = () => (dispatch, getState) => {
+    const { lastFetch } = getState().entities.bugs;
+
+    const diffInMinutes = moment().diff(moment(lastFetch), 'minutes')
+    if (diffInMinutes < 10) return;
+
+    dispatch(apiCallBegan({
+        url,
+        onStart: bugsRequested.type,
+        onSuccess: bugsReceived.type,
+        onError: bugsRequestFailed.type
+        //onError: actions.apiCallFailed.type
+    }));
+}
+
+
 
 
 
